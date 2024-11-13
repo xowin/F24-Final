faf7ef6662e2ffc0547043204994186f38507190
Admin <admin@Admins-Air-8.attlocal.net>
2024-11-12 17:57:48 -0800

    first commit

--- songs.php
+++ songs.php
@@ -1,54 +1,73 @@
-<?php 
+<?php
 $songs = [
-    ['cover' => '',
-    'title' => '',
-    'album' => '',
-     'artist' => '', 
-     'duration' => ''],
-    ['cover' => '',
-    'title' => '',
-    'album' => '',
-    'artist' => '',
-    'duration' => ''],
-    ['cover' => '',
-    'title' => '',
-    'album' => '',
-    'artist' => '',
-    'duration' => ''],
-    ['cover' => '',
-    'title' => '',
-    'album' => '',
-    'artist' => '',
-    'duration' => ''],
-    ['cover' => '',
-    'title' => '',
-    'album' => '',
-    'artist' => '',
-    'duration' => ''],
-    ['cover' => '',
-    'title' => '',
-    'album' => '',
-    'artist' => '',
-    'duration' => ''],
-    ['cover' => '',
-    'title' => '',
-    'album' => '',
-    'artist' => '',
-    'duration' => ''],
-    ['cover' => '',
-    'title' => '',
-    'album' => '',
-    'artist' => '',
-    'duration' => ''],
-    ['cover' => '',
-    'title' => '',
-    'album' => '',
-    'artist' => '',
-    'duration' => ''],
-    ['cover' => '',
-    'title' => '',
-    'album' => '',
-    'artist' => '',
-    'duration' => ''],
+    [
+        'cover' => 'Red_Like_Roses_Cover.jpg',
+        'title' => 'Red Like Roses (Red Trailer)',
+        'album' => 'Songs Part One',
+        'artist' => 'Jeff Williams, Casey Lee Williams',
+        'duration' => '3:12'
+    ],
+    [
+        'cover' => 'fireflies.jpeg',
+        'title' => 'Fireflies',
+        'album' => 'Ocean Eyes',
+        'artist' => 'Owl City',
+        'duration' => '3:35'
+    ],
+    [
+        'cover' => 'wolf.jpg',
+        'title' => 'Running with the Wolves - WolfWalkers Version',
+        'album' => 'WolfWalkers (Original Motion Picture Soundtrack)',
+        'artist' => 'AURORA',
+        'duration' => '3:35'
+    ],
+    [
+        'cover' => 'kiss.jpeg',
+        'title' => 'Kiss the Sky (from The Wild Robot)',
+        'album' => 'Kiss the Sky (from The Wild Robot)',
+        'artist' => 'Maren Morris',
+        'duration' => '3:36'
+    ],
+    [
+        'cover' => 'Enemy_Imagine_Dragons.jpg',
+        'title' => 'Enemy (with JID) - from the series Arcane League of Legends',
+        'album' => 'Enemy (with JID) - [from the series Arcane League of Legends]',
+        'artist' => 'Imagine Dragons',
+        'duration' => '2:25'
+    ],
+    [
+        'cover' => '',
+        'title' => "Joke's On You",
+        'album' => "Joke's On You",
+        'artist' => 'Charlotte Lawrence',
+        'duration' => '3:11'
+    ],
+    [
+        'cover' => '',
+        'title' => 'Stop And Stare',
+        'album' => 'Dreaming Out Loud',
+        'artist' => 'OneRepublic',
+        'duration' => '3:44'
+    ],
+    [
+        'cover' => '',
+        'title' => 'Chlorine',
+        'album' => 'Trench',
+        'artist' => 'Twenty One Pilots',
+        'duration' => '5:24'
+    ],
+    [
+        'cover' => 'Idol.jpeg',
+        'title' => 'アイドル',
+        'album' => 'アイドル',
+        'artist' => 'YoASOBI',
+        'duration' => '3:46'
+    ],
+    [
+        'cover' => '',
+        'title' => 'Campfire Song Song',
+        'album' => "SpongeBob's Greatest Hits",
+        'artist' => 'Spongebob Squarepants',
+        'duration' => '0:57'
+    ],
 ]
-?>
