3c9d6435791c7063abcedbf0607789e885231edd
Christian Rodrigues <118519219+xowin@users.noreply.github.com>
2024-11-14 16:57:31 -0800

    few changes, Ethan fix

--- index.php
+++ index.php
@@ -9,9 +9,12 @@
     rel="stylesheet" 
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" 
     crossorigin="anonymous">
-  <link rel="stylesheet" href="styles/stylesheet.css" />
+    <!-- Bootstrap icons -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" />
+    <!-- Custom styles -->
+  <link rel="stylesheet" href="styles/style.css" />
 </head>
-<body class="bg-dark text-light">
+<body class="bg-dark">
     <!-- playlist header -->
      <header class="container-fluid">
         <div class="row justify-content-center">
